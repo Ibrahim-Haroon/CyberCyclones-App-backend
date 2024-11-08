061825fab95dde7c8a5cc8e38a95898c0a70cfaf
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-11-08 18:48:42 -0500

    Files cleanup and project configuration updates:
    - Cleaned up cache files for settings, urls, and wsgi.
    - Updated ASGI and WSGI configs for project naming consistency.
    - Corrected settings for SECRET_KEY and DEBUG variables.
    - Updated root view and added health check endpoint in URLs config.

--- src/cybercyclones/urls.py
+++ src/cybercyclones/urls.py
@@ -1,22 +1,34 @@
-"""
-URL configuration for cybercycl project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.http import HttpResponse, JsonResponse
 from django.urls import path
 
+
+def root_view(request) -> HttpResponse:
+    """
+    @rtype: HttpResponse
+    @param request: request object
+    @return: response for root view
+    """
+    try:
+        return HttpResponse("Hello! You're at the root of the Cyber Clones server.", status=200)
+    except Exception as e:
+        return HttpResponse(f"error: {str(e)}", status=500)
+
+
+def health_check(request) -> HttpResponse:
+    """
+    :rtype: JsonResponse
+    :param request: request object
+    :return: response for health check
+    """
+    try:
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(f"error: {str(e)}", status=500)
+
+
 urlpatterns = [
+    path('', root_view, name='root'),
     path('admin/', admin.site.urls),
+    path('health/', health_check, name='health-check')
 ]
