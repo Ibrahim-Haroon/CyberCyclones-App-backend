4bdb2ff8c83021ce4ca65bdb3b236a7922ee17ef
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-11-11 20:24:25 -0500

    Update settings and URLs in cybercyclones application
    - Added 'src' to INSTALLED_APPS in settings.py
    - Imported necessary modules and controllers in urls.py
    - Defined routes using routers and registered controllers
    - Added endpoint paths for authentication, discoveries, leaderboard, points, skins, and users
    - Included API routes under 'api/v1/' for versioning the API.

--- src/cybercyclones/urls.py
+++ src/cybercyclones/urls.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from django.http import HttpResponse
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from src.rest.auth_controller import AuthController
+from src.rest.discovery_controller import DiscoveryController
+from src.rest.leaderboard_controller import LeaderboardController
+from src.rest.points_controller import PointsController
+from src.rest.skin_controller import SkinController
+from src.rest.user_controller import UserController
+
+router = routers.DefaultRouter()
+
+router.register(r'auth', AuthController, basename='auth')
+router.register(r'discoveries', DiscoveryController, basename='discoveries')
+router.register(r'leaderboard', LeaderboardController, basename='leaderboard')
+router.register(r'points', PointsController, basename='points')
+router.register(r'skins', SkinController, basename='skins')
+router.register(r'users', UserController, basename='users')
 
 
 def root_view(request) -> HttpResponse:
@@ -30,5 +46,6 @@ def health_check(request) -> HttpResponse:
 urlpatterns = [
     path('', root_view, name='root'),
     path('admin/', admin.site.urls),
-    path('health/', health_check, name='health-check')
+    path('health/', health_check, name='health-check'),
+    path('api/v1/', include(router.urls))
 ]
