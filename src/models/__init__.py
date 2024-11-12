ef37a10ba3e2452413404d6c3ed3933822180296
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-11-11 23:54:32 -0500

    ``` Updated Django settings, added app configuration, models, and authentication settings. - Added AppConfig class for 'src' app - Updated database settings to use PostgreSQL and environment variables - Set upload and parser max sizes - Configured JWT authentication and token lifetimes - Defined custom user model - Added logging configuration ```

---
+++ src/models/__init__.py
@@ -0,0 +1,7 @@
+from .user import User
+from .items import Item
+from .skin import Skin
+from .user_discoveries import UserDiscovery
+from .user_skins import UserSkin
+
+__all__ = ['User', 'Item', 'Skin', 'UserDiscovery', 'UserSkin']
